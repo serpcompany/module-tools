38c8b43631b141c6d58db0a0353f8276c2f89cbb
dvnschmchr <devin@serp.ai>
2023-09-19 08:49:15 -0700

    checkpoint

---
+++ tools_done/countCharactersNoWhitespace/countCharactersNoWhitespace.js
@@ -0,0 +1,26 @@
+// countCharactersNoWhitespace.js
+
+function countCharactersNoWhitespace(text) {
+    const charactersNoWhitespace = text.replace(/\s+/g, '');
+    return charactersNoWhitespace.length;
+}
+
+// button click handler
+function handleButtonClickCountCharactersNoWhitespace() {
+    const textarea = document.getElementById('input-count-characters-no-whitespace');
+    const count = countCharactersNoWhitespace(textarea.value);
+    document.getElementById('result-count-characters-no-whitespace').innerText = "Result: " + count;
+}
+
+// event listener
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById('button-count-characters-no-whitespace');
+    btn.addEventListener('click', handleButtonClickCountCharactersNoWhitespace)
+})
+
+
+// event listener
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById('button-count-characters-no-whitespace');
+    btn.addEventListener('click', handleButtonClickCountCharactersNoWhitespace);
+});
