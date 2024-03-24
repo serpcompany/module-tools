76497ed414afa0d6b0d84af856aa5aad7073cab9
devinschumacher <devin@serp.co>
2024-03-23 17:44:39 -0700

    rename folders and add new tool merge-words

--- tools_in_progress/djangoKeygen/gif_to_png/gif_to_png.js
+++
@@ -1,4 +0,0 @@
-
-document.getElementById('convertButton').addEventListener('click', function() {
-    alert('Conversion logic for Gif To Png goes here.');
-});
--- tools_in_progress/getSitemapUrlsFromUrl/gif_compressor/gif_compressor.js
+++
@@ -1,4 +0,0 @@
-
-document.getElementById('convertButton').addEventListener('click', function() {
-    alert('Conversion logic for Gif Compressor goes here.');
-});
